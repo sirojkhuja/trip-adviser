1e252131a2aafb3f18007d0c64b15c25ed399a63
Sirojkhuja Muminkhodjaev <sirojxuja@gmail.com>
2021-12-20 11:50:12 +0500

    Some database connection changes

---
+++ api/configs/setup-example.php
@@ -0,0 +1,11 @@
+<?php
+
+    define('DATABASEHOST','localhost'); //database host name or IP
+
+    define('DATABASEUSER','root'); //database user name
+
+    define('DATABASEPASSWORD',''); // database user password
+
+    define('DATABASENAME','database'); //database name
+
+?>
